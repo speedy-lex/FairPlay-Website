88b221003314a6151670cd13c40a14cc3acd4b57
spectra <nayelfourreau@gmail.com>
2025-07-31 18:35:22 +0200

    Font loading correction (bug with the VPS)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,10 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
       <body className="antialiased">
         <Main />
         <NextScript />
