380fa3c1313ff190e9241de74071c6a8153fd76f
spectra <nayelfourreau@gmail.com>
2025-09-04 22:52:17 +0200

    corrected /mychannel Images error

--- next.config.ts
+++ next.config.ts
@@ -8,11 +8,21 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
 
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "tilbgoymzrpnhzlqsvgj.supabase.co",
+        pathname: "/storage/v1/object/public/avatars/**",
+      },
+    ],
+  },
+
   async rewrites() {
     return [
-      { source: '/home', destination: '/index.html' },
-      { source: '/', destination: '/index.html' },
-      { source: "/cgu", destination: '/cgu.html' },
+      { source: "/home", destination: "/index.html" },
+      { source: "/", destination: "/index.html" },
+      { source: "/cgu", destination: "/cgu.html" },
       { source: "/contributors", destination: "/contributors.html" },
       { source: "/roadmap", destination: "/roadmap.html" },
     ];
